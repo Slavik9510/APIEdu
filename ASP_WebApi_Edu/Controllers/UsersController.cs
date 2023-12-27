775bce4130e1d9e529d67afc1221a46f07a57871
Sviatoslav Ohorodnyk <sviatoslav.ohorodnyk.pz.2021@lpnu.ua>
2023-12-27 14:24:38 +0200

    first commit

---
+++ ASP_WebApi_Edu/Controllers/UsersController.cs
@@ -0,0 +1,36 @@
+﻿using ASP_WebApi_Edu.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASP_WebApi_Edu.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var users = _context.Users.ToList();
+
+            return Ok(users);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get([FromRoute] int id)
+        {
+            var user = _context.Users.SingleOrDefault(u => u.Id == id);
+            if (user == null)
+            {
+                return BadRequest();
+            }
+            return Ok(user);
+        }
+    }
+}
---
+++ ASP_WebApi_Edu/Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using ASP_WebApi_Edu.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASP_WebApi_Edu.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
